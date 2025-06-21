85f847ccf100be6f08fc65bf0385ba5318cb78f7
Ariel Barboza <ariel@arielbarboza.com>
2025-06-21 02:23:15 -0400

    Added all astro;

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://hilbar.llc', // Your custom domain
+  base: '/', // Since you're using a custom domain
+  output: 'static',
+  build: {
+    assets: 'assets'
+  },
   integrations: [tailwind()]
 });
