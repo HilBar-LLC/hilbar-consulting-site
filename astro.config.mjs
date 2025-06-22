0ad7d7b3f21be4d04946590905e5d277d447c357
Ariel Barboza <ariel@arielbarboza.com>
2025-06-22 01:32:57 -0400

    Added site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://hilbar.llc', // Your custom domain
-  base: '/', // Since you're using a custom domain
-  output: 'static',
-  build: {
-    assets: 'assets'
-  },
-  integrations: [tailwind()]
+  site: 'https://hilbar.llc', // Required for sitemap to work
+  integrations: [
+    tailwind(),
+    sitemap(), // <-- Add this
+  ],
 });
